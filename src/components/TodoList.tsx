2ae4c921ab7f954c28c7f982ce63067ce45256e0
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-08-03 15:11:14 +0200

    I2/J7 Add button to add new todo

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Todo from './Todo';
+import AddTodoForm from './AddTodoForm';
 
 type TodoListProps = {
   title: string
@@ -13,6 +14,7 @@ class TodoList extends React.Component<TodoListProps, TodoListState> {
   constructor(props: TodoListProps) {
     super(props);
     this.state = { listTodos: [{ name: 'Faire une Todolist', done: true }, { name: 'Faire un bouton pour ajouter des todos', done: false }] };
+    this.handleAdd = this.handleAdd.bind(this)
   }
 
   render() {
@@ -22,8 +24,15 @@ class TodoList extends React.Component<TodoListProps, TodoListState> {
         <ul style={{margin: 0, padding: 0}}>
           {this.state.listTodos.map((todo) => <Todo name={todo.name} done={todo.done} />)}
         </ul>
+        <AddTodoForm handleAdd={this.handleAdd} />
       </div>
     );
+  }
+
+  handleAdd(name: string): void  {
+    const actualTodos = this.state.listTodos;
+    actualTodos.push({name: name, done: false})
+    this.setState({listTodos: actualTodos})
   }
 }
 
