7b0fafa7c6d870f2eead40c895a057fb302e4050
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-08-04 16:38:06 +0200

    I2/J9 Add router with basic Login component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { blue, blueGrey, red } from '@material-ui/core/colors';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Box from '@material-ui/core/Box';
 import AppLayout from './AppLayout';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+export const history = createBrowserHistory();
 
 const theme = createMuiTheme({
   palette: {
@@ -16,12 +20,14 @@ const theme = createMuiTheme({
 
 function App() {
   return (
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Box>
-            <AppLayout/>
-          </Box>
-        </ThemeProvider>
+    <Router history={history}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Box>
+          <AppLayout/>
+        </Box>
+      </ThemeProvider>
+    </Router>
   );
 }
 
